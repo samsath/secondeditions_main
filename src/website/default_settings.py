e3126b40e02e86c3959649c2c117d6c58e366176
Sam Hipwell <samhipwell@gmail.com>
2015-08-09 15:46:45 +0100

    Changes to how the front page should be like

--- src/website/default_settings.py
+++ src/website/default_settings.py
@@ -66,9 +66,11 @@ SITE_ID = 1
 # --------------------------
 
 MEDIA_ROOT = '/home/secondeditions/public_html/media'
+#MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = '/home/secondeditions/public_html/static'
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'media', 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
